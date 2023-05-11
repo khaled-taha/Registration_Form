50619d545725ca145f8ae823c163004af757d6d1
Khaled Taha <khaledtaha105@gmail.com>
2023-05-11 22:05:08 +0300

    Update

--- src/main/java/com/registrationform/config/security/CorsConfig.java
+++ src/main/java/com/registrationform/config/security/CorsConfig.java
@@ -9,7 +9,7 @@ public class CorsConfig implements WebMvcConfigurer{
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/users").allowedMethods("POST").allowedOrigins("https://userform-homepage.herokuapp.com")
+        registry.addMapping("/users").allowedMethods("POST").allowedOrigins("*")
                 .allowedHeaders("*");
     }
 }
