e2c4f9a1e7c8d2c45b92e4cc682869dda77c1bce
Khaled Taha <khaledtaha105@gmail.com>
2023-05-16 04:34:45 +0300

    Update

--- src/main/java/com/registrationform/config/security/CorsConfig.java
+++ src/main/java/com/registrationform/config/security/CorsConfig.java
@@ -11,7 +11,7 @@ public class CorsConfig implements WebMvcConfigurer{
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/users")
                 .allowedMethods("POST")
-                .allowedOrigins("https://www.ahbabkhatamelmorsalen.com")
+                .allowedOrigins("*")
                 .allowedHeaders("*");
     }
 }
