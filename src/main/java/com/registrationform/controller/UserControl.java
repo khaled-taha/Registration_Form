4fc504e057903f3aa1b0f830c6ed024bfaabd03a
Khaled Taha <khaledtaha105@gmail.com>
2023-05-17 21:25:13 +0300

    Update

--- src/main/java/com/registrationform/controller/UserControl.java
+++ src/main/java/com/registrationform/controller/UserControl.java
@@ -29,7 +29,7 @@ public class UserControl {
 
     @GetMapping("/Register/state")
     public String state() {
-        return "redirect:/";
+        return "/";
     }
 
     @PostMapping("/api/users")
